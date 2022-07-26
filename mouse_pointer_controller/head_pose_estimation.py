11a044fec7bde92308df0ae32df4907ff3d9b4c6
Wajih Ouertani <wajih.ouertani@gmail.com>
2022-07-26 09:03:19 +0200

    add HeadPose class

--- mouse_pointer_controller/head_pose_estimation.py
+++ mouse_pointer_controller/head_pose_estimation.py
@@ -5,12 +5,41 @@ HeadPoseEstimator class (OpenVino model wrapper)
  * @email wajih.ouertani@gmail.com
  """
 
+from dataclasses import dataclass
+from operator import itemgetter
 from typing import Dict
 
 import numpy as np
 from mouse_pointer_controller.single_image_openvino_model import (
     SingleImageOpenVinoModel,
 )
+
+
+@dataclass
+class HeadPose:
+
+    yaw: float
+    pitch: float
+    roll: float
+
+    @classmethod
+    def from_head_pose_estimator_output(cls, head_pose_dict: Dict[str, np.array]):
+        """Build HeadPose from  a pose estimator output
+           Example:
+            {'angle_p_fc': array([[4.490898]], dtype=float32),
+            'angle_r_fc': array([[-1.581182]], dtype=float32),
+            'angle_y_fc': array([[-1.8917689]], dtype=float32)
+        }
+
+        """
+        yaw, pitch, roll = itemgetter("angle_y_fc", "angle_p_fc", "angle_r_fc")(
+            {key: value.flatten()[0] for key, value in head_pose_dict.items()}
+        )
+        return HeadPose(yaw=yaw, pitch=pitch, roll=roll)
+
+    @property
+    def as_array(self):
+        return np.array([self.yaw, self.pitch, self.roll])
 
 
 class HeadPoseEstimator(SingleImageOpenVinoModel):
