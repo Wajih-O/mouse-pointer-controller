c27d7a6b79db87ec6ef8327d3693bd6bf70ab5ed
Wajih Ouertani <wajih.ouertani@gmail.com>
2022-07-29 22:58:03 +0200

    add GazeEstimation and GazeEstimation

--- mouse_pointer_controller/gaze_estimation.py
+++ mouse_pointer_controller/gaze_estimation.py
@@ -5,14 +5,42 @@ GazeEstimation class (OpenVino model wrapper)
  * @email wajih.ouertani@gmail.com
  """
 
+from dataclasses import dataclass
+from operator import le
+
 import numpy as np
 
 from mouse_pointer_controller.head_pose_estimation import HeadPose, HeadPoseEstimator
-from mouse_pointer_controller.landmarks_regression import LandmarksRegression
+from mouse_pointer_controller.landmarks_regression import (
+    EyesLandmarks,
+    LandmarksRegression,
+)
 from mouse_pointer_controller.openvino_model import (
     OpenVinoModel,
     preprocess_image_input,
 )
+
+
+@dataclass
+class GazeEstimation:
+    x: float
+    y: float
+    z: float
+
+    @classmethod
+    def from_array(cls, gaze_vector: np.ndarray):
+        """Build a GazeEstimation object from a gaze_vector"""
+        if gaze_vector.shape != (3,):
+            raise ValueError("expect a 3d vector")
+        x, y, z = gaze_vector
+        return GazeEstimation(x=x, y=y, z=z)
+
+
+@dataclass
+class GazeEstimationResult:
+    head_pose: HeadPose
+    eyes_landmarks: EyesLandmarks
+    gaze: GazeEstimation
 
 
 class GazeEstimator(OpenVinoModel):
@@ -25,6 +53,22 @@ class GazeEstimator(OpenVinoModel):
 
     landmarks_regressor: LandmarksRegression
     head_pose_estimator: HeadPoseEstimator
+
+    def infer(
+        self,
+        head_pose: HeadPose,
+        left_eye_image: np.ndarray,
+        right_eye_image: np.ndarray,
+    ):
+        """Infer gaze estimation from head pose and left, right eyes images"""
+
+        return self.network.infer(
+            {
+                "head_pose_angles": head_pose.as_array,
+                "left_eye_image": left_eye_image,
+                "right_eye_image": right_eye_image,
+            }
+        )
 
     def estimate_gaze(self, face_image: np.ndarray):
         """Estimate gaze from a face image using the landmarks regression
@@ -53,10 +97,9 @@ class GazeEstimator(OpenVinoModel):
             self.network.input_info.get("right_eye_image").input_data.shape,
         )
 
-        return self.network.infer(
-            {
-                "head_pose_angles": head_pose.as_array,
-                "left_eye_image": left_eye_crop,
-                "right_eye_image": right_eye_crop,
-            }
+        gaze_vector = self.infer(
+            head_pose=head_pose,
+            left_eye_image=left_eye_crop,
+            right_eye_image=right_eye_crop,
         )
+        return gaze_vector
